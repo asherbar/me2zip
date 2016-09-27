20edfc6376140fcb6c17488a71689bf940831a7f
asher.bar.eitan <asher@redislabs.com>
2016-09-27 15:53:01 +0300

    Formalized context load

--- me2zip_main/views.py
+++ me2zip_main/views.py
@@ -2,24 +2,29 @@ import os
 
 from django.shortcuts import render
 
-
 _GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 
 
-def index(request):
-    context = {
-        'latitude': 0, 'longitude': 0, 'gm_api_key': _GOOGLE_MAPS_API_KEY
+class _Context(dict):
+    REQUIRED_KEYS_DEFAULTS = {
+        'latitude': None, 'longitude': None, 'gm_api_key': _GOOGLE_MAPS_API_KEY, 'search_id': None, 'resolved_zip': None
     }
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for key, default in self.REQUIRED_KEYS_DEFAULTS.items():
+            if key not in self:
+                self[key] = default
+
+
+def index(request):
+    context = _Context()
     if request.method == 'POST':
         search_id = request.POST.get('textfield', None)
         context['search_id'] = search_id
-    else:
-        context['search_id'] = ''
     return render(request, 'me2zip_main/home.html', context=context)
 
 
 def get_zip_from_lat_long(request, latitude, longitude):
-    context = {
-        'latitude': latitude, 'longitude': longitude, 'search_id': '', 'gm_api_key': _GOOGLE_MAPS_API_KEY
-    }
+    context = _Context(latitude=latitude, longitude=longitude)
     return render(request, 'me2zip_main/home.html', context=context)
