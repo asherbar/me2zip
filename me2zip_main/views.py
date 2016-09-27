f8088667c95b70d6207649783d15a118af98680c
asher.bar.eitan <asher@redislabs.com>
2016-09-27 15:29:07 +0300

    Removed explicit google maps key

--- me2zip_main/views.py
+++ me2zip_main/views.py
@@ -1,21 +1,25 @@
+import os
+
 from django.shortcuts import render
 
 
+_GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
+
+
 def index(request):
+    context = {
+        'latitude': 0, 'longitude': 0, 'gm_api_key': _GOOGLE_MAPS_API_KEY
+    }
     if request.method == 'POST':
         search_id = request.POST.get('textfield', None)
-        context = {
-            'search_id': search_id, 'latitude': 0, 'longitude': 0
-        }
+        context['search_id'] = search_id
     else:
-        context = {
-            'search_id': '', 'latitude': 0, 'longitude': 0
-        }
+        context['search_id'] = ''
     return render(request, 'me2zip_main/home.html', context=context)
 
 
 def get_zip_from_lat_long(request, latitude, longitude):
     context = {
-        'latitude': latitude, 'longitude': longitude, 'search_id': '',
+        'latitude': latitude, 'longitude': longitude, 'search_id': '', 'gm_api_key': _GOOGLE_MAPS_API_KEY
     }
     return render(request, 'me2zip_main/home.html', context=context)
