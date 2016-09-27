a03154bd1b196245f4dc08e16079e04d5b2f767e
asher.bar.eitan <asher@redislabs.com>
2016-09-27 12:11:05 +0300

    Bug fix: map was not centered at user's location

--- me2zip_main/static/js/position_map.js
+++ me2zip_main/static/js/position_map.js
@@ -6,27 +6,27 @@
 var DEFAULT_LAT = 32.776800;
 var DEFAULT_LONG = 35.023128;
 
-function centerMap(map_elem_id, latitude=null, longitude=null) {
+function centerMap(map_elem_id, latitude=null, longitude=null, set_url=false) {
+    var errMsg = '';
     if (latitude == null || longitude == null) {
+        centerMap(map_elem_id, DEFAULT_LAT, DEFAULT_LONG, false);
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 latitude = position.coords.latitude;
                 longitude = position.coords.longitude;
+                centerMap(map_elem_id, latitude, longitude, set_url);
             }, function() {
-                var errMsg = "Error: The Geolocation service failed.";
+                alert("Error: The Geolocation service failed.");
+                centerMap(map_elem_id, DEFAULT_LAT, DEFAULT_LONG, set_url);
             });
         } else {
-            var errMsg = "Error: Your browser doesn\'t support geolocation.";
+            alert("Error: Your browser doesn\'t support geolocation.");
+            centerMap(map_elem_id, DEFAULT_LAT, DEFAULT_LONG, set_url);
         }
+        return;
     }
-    if (latitude == null || longitude == null) {
-        var infoWindowContent = errMsg;
-        latitude = DEFAULT_LAT;
-        longitude = DEFAULT_LONG;
-    } else {
-        var infoWindowContent = "Location found.";
-    }
+    var infoWindowContent = "Location found:" + latitude + "," + longitude;
     var pos = {lat: latitude, lng: longitude};
     var map = new google.maps.Map(document.getElementById(map_elem_id), {
         center: pos,
@@ -35,28 +35,9 @@ function centerMap(map_elem_id, latitude=null, longitude=null) {
     var infoWindow = new google.maps.InfoWindow({map: map});
     infoWindow.setPosition(pos);
     infoWindow.setContent(infoWindowContent);
-
-
-//
-//    // Try HTML5 geolocation.
-//    if (navigator.geolocation) {
-//      navigator.geolocation.getCurrentPosition(function(position) {
-//        var pos = {
-//          lat: position.coords.latitude,
-//          lng: position.coords.longitude
-//        };
-//
-//        infoWindow.setPosition(pos);
-//        infoWindow.setContent('Location found.');
-//        map.setCenter(pos);
-//        window.location.href = '/lat='+pos["lat"]+"&long="+pos["lng"] ; //relative to domain
-//      }, function() {
-//        handleLocationError(true, infoWindow, map.getCenter());
-//      });
-//    } else {
-//      // Browser doesn't support Geolocation
-//      handleLocationError(false, infoWindow, map.getCenter());
-//    }
+    if (set_url) {
+        window.location.href = '/lat=' + latitude + '&long=' + longitude;
+    }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
