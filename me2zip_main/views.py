63ea22811718936fd05b3fe87523d477a645c4b0
asher.bar.eitan <asher@redislabs.com>
2016-09-28 14:44:42 +0300

    Clean up: improved html appearance

--- me2zip_main/views.py
+++ me2zip_main/views.py
@@ -1,13 +1,17 @@
 import os
 
 from django.shortcuts import render
+from zip_by_address.address_by_coordinates.address_by_coordinates_localizations.israel.\
+    address_by_coordinates_resolver_israel import AddressByCoordinatesResolverIsrael
+from zip_by_address.zip_by_address_localizations.israel.zip_by_address_israel import ZipByAddressIsrael
 
 _GOOGLE_MAPS_API_KEY = os.environ['GOOGLE_MAPS_API_KEY']
 
 
 class _Context(dict):
     REQUIRED_KEYS_DEFAULTS = {
-        'latitude': None, 'longitude': None, 'gm_api_key': _GOOGLE_MAPS_API_KEY, 'search_id': None, 'resolved_zip': None
+        'latitude': None, 'longitude': None, 'gm_api_key': _GOOGLE_MAPS_API_KEY, 'search_id': None,
+        'resolved_zip': None, 'resolved_address': None,
     }
 
     def __init__(self, **kwargs):
@@ -26,5 +30,8 @@ def index(request):
 
 
 def get_zip_from_lat_long(request, latitude, longitude):
-    context = _Context(latitude=latitude, longitude=longitude)
+    resolved_address = AddressByCoordinatesResolverIsrael(latitude=latitude, longitude=longitude).resolve_address()
+    resolved_zip = ZipByAddressIsrael(resolved_address).resolve_zip()
+    context = _Context(latitude=latitude, longitude=longitude, resolved_zip=resolved_zip,
+                       resolved_address=resolved_address)
     return render(request, 'me2zip_main/home.html', context=context)
