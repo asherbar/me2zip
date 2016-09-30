67be2c577316d0f919e6eb889887a48b82ed6ee7
asher <asherbare@gmail.com>
2016-10-01 00:57:26 +0300

    Added feature: Map marker is draggable

--- me2zip_main/static/js/position_map.js
+++ me2zip_main/static/js/position_map.js
@@ -3,15 +3,25 @@ var DEFAULT_LAT = 32.776800;
 var DEFAULT_LONG = 35.023128;
 
 function centerMap(mapElemId, latitude=DEFAULT_LAT, longitude=DEFAULT_LONG, onMapCentered=null) {
-    var infoWindowContent = "Location found: " + latitude + ", " + longitude;
     var pos = {lat: latitude, lng: longitude};
     var map = new google.maps.Map(document.getElementById(mapElemId), {
         center: pos,
         zoom: 15
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(infoWindowContent);
+    // Place a draggable marker on the map
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        draggable: true,
+        title: "Drag me!"
+    });
+
+    marker.addListener('dragend', function() {
+          pos = marker.getPosition();
+          window.location.href = '/mapcoords=' + pos.lat() + ',' + pos.lng();
+    });
+
     if (onMapCentered) {
         onMapCentered(latitude, longitude)
     }
