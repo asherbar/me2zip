761e20190b28ea990e153be10c7c34dd693af6e8
asher <asherbare@gmail.com>
2016-10-02 01:06:10 +0300

    Added whitenoise as requirement

--- me2zip/settings.py
+++ me2zip/settings.py
@@ -124,9 +124,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/me2zip_main/static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    # os.path.join(PROJECT_ROOT, 'static'),
+    os.path.abspath(os.path.join(PROJECT_ROOT, '..', 'me2zip_main', 'static')),
 )
